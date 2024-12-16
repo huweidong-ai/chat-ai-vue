2bc6e227489f3299db28b9cdf3265d4b0dc49f59
hello <huweidong@empowerdata.cn>
2024-12-16 14:16:25 +0800

    refactor(router): 移除新建对话功能
    - 删除了 src/router/index.js 中的新建对话路由- 删除了 src/components/NewChat.vue 文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import ConversationListAndChat from '../components/ConversationListAndChat.vue';
-import NewChat from '../components/NewChat.vue';
 
 const routes = [
     {
         path: '/',
         name: 'ConversationListAndChat',
         component: ConversationListAndChat
-    },
-    {
-        path: '/new-chat',
-        name: 'NewChat',
-        component: NewChat
     }
 ];
 
